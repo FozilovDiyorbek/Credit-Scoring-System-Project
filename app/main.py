7d4bfd77622a16f258e4b023be51694129b25457
FozilovDiyorbek <diyorbekfozilov011@gmail.com>
2025-10-14 17:28:43 +0500

    Added files

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from app.schemes import CreditData
+import joblib
+import pandas as pd
+
+app = FastAPI(title="Credit Risk Scoring API")
+
+model = joblib.load("models/best_model.pkl")
+
+
+@app.post("/predict")
+def predict(data: CreditData):
+    df = pd.DataFrame([data.dict()])
+
+    prediction = model.predict(df)[0]
+    proba = model.predict_proba(df)[0][1]
+
+    return {
+        "risk_prediction": int(prediction),
+        "risk_probability": round(float(proba), 3)
+    }
